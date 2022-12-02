bfbdd225795fa8f68cc1f0f7fd5c267538e88ec8
owenavon <89999663+owenavon@users.noreply.github.com>
2022-12-01 22:58:43 -0500

    Organized code & Added reset function
    void(* resetFunc) (void) = 0; gets called at the end of the array to restart the program cycle

--- final/hapFreq/hapFreq.ino
+++ final/hapFreq/hapFreq.ino
@@ -1,52 +1,62 @@
 #include "pitches.h"
 
-#define RESISTOR 10000 //This should be the same value of the used resistor  
-#define RUBBERCORDPIN A0  //This is the pin where the cord is connected tp
+#define RESISTOR 10000 // Same value as resistor
+#define RUBBER_CORD A0
 #define DLED 13
 
-const int threshold = 890;  // an arbitrary threshold level that's in the range of the analog input
+const int threshold = 880;  // an arbitrary threshold level that's in the range of the analog input
 
-const int motorPin = 3; //motor transistor is connected to pin 3
-const int headPhones = 9; //buzzer to arduino pin 9
-const int maxNotes = 2;
+const int motorPin = 3;
+const int headPhones = 9; 
+const int maxNotes = 10;
 
 // notes array
-int notes[]={NOTE_B0,NOTE_E1,NOTE_A1,NOTE_E5,NOTE_FS6,NOTE_GS7,NOTE_CS8,NOTE_B7,NOTE_CS7,NOTE_FS6};
+int notes[10]={NOTE_B0,NOTE_E1,NOTE_A1,NOTE_E5,NOTE_FS6,NOTE_GS7,NOTE_CS8,NOTE_B7,NOTE_CS7,NOTE_FS6};
 
 // current note
 int currentNote = 0;
 
-void setup() { 
+// Stretch Sensor
+int ambiguousVal; 
+int raw = 0; // Raw input value
+int vin = 5; // Store input voltage - 5
+float vout = 0; // Store output voltage 
+float resistor = 10; // Variable to store the resistor value 
+float buffer = 0; // Buffer variable for calculation
+
+void(* resetFunc) (void) = 0; // Decleares reset function at address 0
+
+void setup() { // Runs once, and sets pins to OUTPUT
   pinMode(DLED, OUTPUT);
   pinMode(motorPin, OUTPUT);
-  pinMode(headPhones, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(headPhones, OUTPUT); 
   Serial.begin(9600);
 }
 
 void vibeSound() {
-  int analogValue = analogRead(RUBBERCORDPIN); // read the value of the potentiometer:
+  int analogValue = analogRead(RUBBER_CORD); // read the value of the sensor (potentiometer)
 
   // Current chosen note is less then 9
   if (currentNote < maxNotes) {
-    if (analogValue > threshold) { // if the analog value is greater then the threshold, turn on the LED and play sound:
+    if (analogValue > threshold) { // Turn on the LED, motor vibrates, and pause audio output
       noTone(headPhones);
-      currentNote++;
+      currentNote++; // Increments through the array
       digitalWrite(DLED, HIGH); // Ardunio LED
 
       // Vibration Motors
-      int j=0;
+      int i=0;
       do {
-        j++;
+        i++;
         digitalWrite(motorPin, HIGH); //vibrate
         delay(200);  // delay 0.2 second
         digitalWrite(motorPin, LOW);  //stop vibrating
         delay(200); //delay 0.2 second
       }
-      while(j<2);
-      delay (2000); // delay 2 seconds between vibrations
+      while(i<2);
+      delay (1000); // delay 1 second between vibrations
       }
        
-      else {
+      else { // Play current note and turn off LED
         tone(headPhones, notes[currentNote]);
         digitalWrite(DLED, LOW);
       }
@@ -59,28 +69,39 @@ void vibeSound() {
       delay(200);  // delay 0.2 second
       digitalWrite(DLED, LOW); // Ardunio LED
       delay(200);  // delay 0.2 second
-    }
 
-    int value; 
-    int raw = 0; // Raw input value 
-    int vin = 5; // Store input voltage, this should be 5 
-    float vout = 0; // Store output voltage 
-    float refresistor1 = 10; // Variable to store the R1 value 
-    float refresistor2 = 0; //  Value is determined by the voltage that falls across after it has been measured
-    float buffer = 0; // Buffer variable for calculation 
-    value = analogRead(RUBBERCORDPIN); // Read the value that calculates the voltage
-    vout = (5.0 / 1023.0) * value; // Displays 1023 as voltage
-    buffer = (vin / vout) - 1; 
-    refresistor2 = refresistor1 / buffer; 
-    // Serial.print("Analog reading ");  
-    // Serial.println(value); //Print value
-    // Serial.print("Voltage: "); 
-    // Serial.println(vout); // Outputs the information 
-    // Serial.print("Resistance: ");  
-    // Serial.println(refresistor2);   
-    // delay(1000); 
+      int j=0;
+      do {
+        j++;
+        digitalWrite(motorPin, HIGH); //vibrate
+        delay(100);  // delay 0.1 second
+        digitalWrite(motorPin, LOW); // stop vibrating
+        delay(100); //delay 0.1 second
+      }
+      while(j<10); // Vibrates sequence 10 times upon victory
+      delay(2000); // Take 2 seconds to reset
+      resetFunc();
+    }
 }
 
-void loop() { 
+void stretchSensor() {
+  ambiguousVal = analogRead(RUBBER_CORD); // Read the value that calculates the voltage
+  vout = (5.0 / 1023.0) * ambiguousVal; // Displays 1023 as voltage
+  buffer = (vin / vout) - 1;
+    
+  Serial.print("Analog reading ");  
+  Serial.println(ambiguousVal); // Outputs the ambiguous (0-1024) value
+     
+  // Serial.print("Voltage: "); 
+  // Serial.println(vout); // Outputs the volts
+     
+  // Serial.print("Resistance: ");  
+  // Serial.println(resistor); // Outputs the ohms
+       
+  // delay(1000); 
+}
+
+void loop() { // Runs over and over
   vibeSound(); // Calls the vibeSound function
+  stretchSensor(); // Calls the stretchSensor function
 }
